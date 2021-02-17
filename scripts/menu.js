8842bcdcc2d7ee5fea8f4760ba9051e6b36e2ce7
ryan <colry562@student.otago.ac.nz>
2021-02-18 11:33:46 +1300

    a working mobile navigation

---
+++ scripts/menu.js
@@ -0,0 +1,50 @@
+//Let's Create the JavaScript Part. 
+
+const overlay = document.getElementById('overlay');
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const stopScrolling = document.getElementById('stop-scrolling');
+
+closeMenu.style.display = 'none';
+
+document.getElementById('open-menu') .addEventListener('click', function() {
+    overlay.classList.add('show-menu');
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
+    stopScrolling.style.overflow = 'hidden';
+});
+
+document.getElementById('close-menu').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    stopScrolling.style.overflow = 'visible';
+})
+
+document.getElementById('close-menu-link').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    stopScrolling.style.overflow = 'visible';
+})
+
+document.getElementById('close-menu-link1').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    stopScrolling.style.overflow = 'visible';
+})
+
+document.getElementById('close-menu-link2').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    stopScrolling.style.overflow = 'visible';
+})
+
+document.getElementById('close-menu-link3').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    stopScrolling.style.overflow = 'visible';
+})
